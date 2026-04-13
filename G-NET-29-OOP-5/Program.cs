805c81647fd1368dcb3ef23396d3492006da840b
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-04-13 16:14:50 +0200

    Add project files.

---
+++ G-NET-29-OOP-5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_29_OOP_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
