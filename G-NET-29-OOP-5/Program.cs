0fd2532f9ab1dd5242340b95fd4478cb8250ba6e
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-04-13 17:34:13 +0200

    Q1

--- G-NET-29-OOP-5/Program.cs
+++ G-NET-29-OOP-5/Program.cs
@@ -2,6 +2,20 @@
 {
     internal class Program
     {
+
+        #region Q1:
+        //*An interface defines a contract that a class must follow.
+        //  It specifies what a class can do, not how it does it.
+        //*because Concrete Classes lead to Tight Coupling,interfaces 
+        //    provides High flexibility.
+        //* Benefits:
+        //    Polymorphism
+        //    Loose Coupling
+        //    Multiple Inheritance
+        //    make Testing easier.
+
+        #endregion
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
